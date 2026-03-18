8d6655a6cd65d97afab7869e64f769e2f40fc337
Lucas-Heuchert <lucasfheuchert@gmail.com>
2026-03-17 17:49:55 -0700

    Add files via upload

--- tic_tac_toe_alg.py
+++ tic_tac_toe_alg.py
@@ -1,6 +1,5 @@
-# Code was modified from AI generated algorithm
-
 class TicTacToe:
+
     def __init__(self):
         self.EMPTY = 0
         self.HUMAN = 1
@@ -12,21 +11,19 @@ class TicTacToe:
             (0, 4, 8), (2, 4, 6)               # diagonals
         ]
 
-    # def new_board(sel,board):
-    #     return [self.EMPTY] * 9
-
-    # def print_board(self, board):
-    #     def cell(i):
-    #         return board[i] if board[i] != self.EMPTY else str(i)
-    #     print()
-    #     print(f" {cell(0)} | {cell(1)} | {cell(2)} ")
-    #     print("---+---+---")
-    #     print(f" {cell(3)} | {cell(4)} | {cell(5)} ")
-    #     print("---+---+---")
-    #     print(f" {cell(6)} | {cell(7)} | {cell(8)} ")
-    #     print()
-
     def available_moves(self, board):
+        '''
+        Gets and returns an arary of available moves from a NumPy array with
+        robot and human player moves.
+
+        Parameters:
+            board: A Numpy array that has a 0 if the space is empty
+            a 1 if the player has a move in that space, and a 2 if the robot has a
+            move in that space.
+
+        Returns:
+            available_moves: A list of available moves
+        '''
         available_moves = []
         for i,val in enumerate(board):
             if val==0:
@@ -34,28 +31,75 @@ class TicTacToe:
         return available_moves
     
     def winner(self, board):
+        ''' 
+        Returns a 2 if the robot wins, a 1 if the user wins, and none if no one has one.
+
+        Parameters:
+            board: A Numpy array that has a 0 if the space is empty
+            a 1 if the player has a move in that space, and a 2 if the robot has a
+            move in that space.
+        
+        Returns:
+            board[a]: returns the value at index 'a' which will result in a 2 or 1
+        
+        '''
         for a, b, c in self.WIN_LINES:
             if board[a] != self.EMPTY and board[a] == board[b] == board[c]:
                 return board[a]
         return None
 
     def is_draw(self, board):
+        '''
+        Returns True or False based on if there is a draw or not
+
+        Parameters:
+            board: A Numpy array that has a 0 if the space is empty
+            a 1 if the player has a move in that space, and a 2 if the robot has a
+            move in that space.
+        
+        Returns:
+            True or False
+        '''
         return self.winner(board) is None and all(c != self.EMPTY for c in board)
 
     def apply_move(self, board, idx, player):
-        """Mutates board. Returns True if move applied, False if illegal."""
+        '''
+        Mutates the board, Returns True if the move was applied, and False if it was
+        an illegal move.
+
+        Parameters:
+            idx: The index of the move to be applied
+            board: A Numpy array that has a 0 if the space is empty
+            a 1 if the player has a move in that space, and a 2 if the robot has a
+            move in that space.
+            player: the number value of either a 1 for the human or 2 for the robot
+
+        Returns:
+            True or False depending if the move was applied or not
+        '''
         if 0 <= idx < 9 and board[idx] == self.EMPTY:
             board[idx] = player
             return True
         return False
 
     def minimax(self, board, is_robot_turn, depth=0):
-        """
-        Returns a score from ROBOT's perspective:
-          ROBOT win: +10 - depth
-          HUMAN win: depth - 10
-          draw: 0
-        """
+        '''
+        Returns a score from the Robot's perspective:
+            ROBOT win: +10 - depth
+            HUMAN win: depth - 10
+            draw: 0
+        
+        Parameters:
+            board: A Numpy array that has a 0 if the space is empty
+            a 1 if the player has a move in that space, and a 2 if the robot has a
+            move in that space.
+            is_robot_turn: Boolean, True if it is the robots turn and vise versa
+            depth: the amount of turns for a given move sequence
+
+        Returns:
+            best: The best garunteed score assuming both players play perfect
+        '''
+        
         w = self.winner(board)
         if w == self.ROBOT:
             return 10 - depth
@@ -82,6 +126,17 @@ class TicTacToe:
             return best
 
     def best_robot_move(self, moves):
+        '''
+        Returns which cell has the best robot move.
+
+        Parameters:
+            moves: A Numpy array that has a 0 if the space is empty
+            a 1 if the player has a move in that space, and a 2 if the robot has a
+            move in that space.
+
+        Returns:
+            best_move: The cell which has the best robot move.
+        '''
         best_score = -999
         best_move = 20
         board = list(moves)
@@ -97,37 +152,4 @@ class TicTacToe:
         moves[best_move] = 2
         return best_move
 
-    # def get_human_move(self, board):
-    #     while True:
-    #         try:
-    #             idx = int(input("Your move (0-8): ").strip())
-    #             if idx in self.available_moves(board):
-    #                 return idx
-    #             print("That square is not available.")
-    #         except ValueError:
-    #             print("Please enter a number 0-8.")
-
-    # def game_loop(self):
-    #     board = self.new_board()
-    #     turn = self.HUMAN  # human starts; change to self.ROBOT if you want robot first
-
-    #     while True:
-    #         self.print_board(board)
-
-    #         w = self.winner(board)
-    #         if w is not None:
-    #             print(f"{w} wins!")
-    #             break
-    #         if self.is_draw(board):
-    #             print("Draw!")
-    #             break
-
-    #         if turn == self.HUMAN:
-    #             move = self.get_human_move(board)
-    #             self.apply_move(board, move, self.HUMAN)
-    #             turn = self.ROBOT
-    #         else:
-    #             move = self.best_robot_move(board)
-    #             print(f"Robot plays: {move}")
-    #             self.apply_move(board, move, self.ROBOT)
-    #             turn = self.HUMAN
+    
